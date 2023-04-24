fb7f0c509ba6ed57caf8d672cb4fdce3d9ea8427
insolento <maksym.bezverkhnii@gmail.com>
2023-04-24 14:03:20 -0400

    Initial Commit

---
+++ SkyWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyWatch
+//
+//  Created by maksym on 2023-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
